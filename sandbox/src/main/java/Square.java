f337e5b1a6c6efcf46d6ecf573795cd123ddf0ed
AleksandrProSar <104913986+AleksandrProSar@users.noreply.github.com>
2022-05-04 18:58:03 +0400

    Создан проект песочница

---
+++ sandbox/src/main/java/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+
+    public double l;
+
+    public Square(double l) { // добавляем конструктор
+       this.l = l;
+    }
+
+    public  double area() {
+        return this.l * this.l;
+    }
+}
